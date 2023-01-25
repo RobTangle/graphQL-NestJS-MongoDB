8cd486142da9a6270a1b23fe14d5425663544e4f
Robert0101 <rob.tangled@gmail.com>
2023-01-25 19:31:08 -0300

    Crear users module e importarlo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [GraphQLModule.forRoot({ autoSchemaFile: true })],
+  imports: [GraphQLModule.forRoot({ autoSchemaFile: true }), UsersModule],
   controllers: [],
   providers: [],
 })

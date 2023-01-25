bd601f8ea74b52fbb94b47f603d3edc12ee98460
Robert0101 <rob.tangled@gmail.com>
2023-01-25 19:46:10 -0300

    Pasar argumento a los métodos de los servicios

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -13,26 +13,26 @@ export class UsersResolver {
   //transformar esto a async cuando implemente la database
   @Query(() => User, { name: 'user', nullable: true })
   getUser(@Args() getUserArgs: GetUserArgs): User {
-    return this.usersService.getUser();
+    return this.usersService.getUser(getUserArgs);
   }
 
   @Query(() => [User], { name: 'users', nullable: 'items' })
   getUsers(@Args() getUsersArgs: GetUsersArgs): User[] {
-    return this.usersService.getUsers();
+    return this.usersService.getUsers(getUsersArgs);
   }
 
   @Mutation(() => User)
   createUser(@Args('createUserData') createUserData: CreateUserInput): User {
-    return this.usersService.createUser();
+    return this.usersService.createUser(createUserData);
   }
 
   @Mutation(() => User)
   updateUser(@Args('updateUserData') updateUserData: UpdateUserInput): User {
-    return this.usersService.updateUser();
+    return this.usersService.updateUser(updateUserData);
   }
 
   @Mutation(() => User)
   deleteUser(@Args('deleteUserData') deleteUserData: DeleteUserInput): User {
-    return this.usersService.deleteUser();
+    return this.usersService.deleteUser(deleteUserData);
   }
 }

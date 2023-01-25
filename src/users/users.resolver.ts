0de887ce45f513c82306332828be187f5b49746e
Robert0101 <rob.tangled@gmail.com>
2023-01-25 19:31:23 -0300

    Crear los distintos métodos del resolver

---
+++ src/users/users.resolver.ts
@@ -0,0 +1,38 @@
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { GetUserArgs } from './dto/args/get-user.args';
+import { GetUsersArgs } from './dto/args/get-users.args';
+import { CreateUserInput } from './dto/input/create-user.input';
+import { DeleteUserInput } from './dto/input/delete-user.input';
+import { UpdateUserInput } from './dto/input/update-user.input';
+import { User } from './models/user';
+import { UsersService } from './users.service';
+
+@Resolver(() => User)
+export class UsersResolver {
+  constructor(private readonly usersService: UsersService) {}
+  //transformar esto a async cuando implemente la database
+  @Query(() => User, { name: 'user', nullable: true })
+  getUser(@Args() getUserArgs: GetUserArgs): User {
+    return this.usersService.getUser();
+  }
+
+  @Query(() => [User], { name: 'users', nullable: 'items' })
+  getUsers(@Args() getUsersArgs: GetUsersArgs): User[] {
+    return this.usersService.getUsers();
+  }
+
+  @Mutation(() => User)
+  createUser(@Args('createUserData') createUserData: CreateUserInput): User {
+    return this.usersService.createUser();
+  }
+
+  @Mutation(() => User)
+  updateUser(@Args('updateUserData') updateUserData: UpdateUserInput): User {
+    return this.usersService.updateUser();
+  }
+
+  @Mutation(() => User)
+  deleteUser(@Args('deleteUserData') deleteUserData: DeleteUserInput): User {
+    return this.usersService.deleteUser();
+  }
+}

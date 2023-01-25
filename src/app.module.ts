431a9c226798aebff0386e0e43d9b83230a11426
Robert0101 <rob.tangled@gmail.com>
2023-01-25 20:07:18 -0300

    update code for v10

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { UsersModule } from './users/users.module';
-
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 @Module({
-  imports: [GraphQLModule.forRoot({ autoSchemaFile: true }), UsersModule],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      autoSchemaFile: true,
+      driver: ApolloDriver,
+    }),
+    UsersModule,
+  ],
   controllers: [],
   providers: [],
 })

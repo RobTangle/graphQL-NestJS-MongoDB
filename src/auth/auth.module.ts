da5b0f3806c8cb89d29328ba1572394c1a1d1147
Robert0101 <rob.tangled@gmail.com>
2023-01-26 11:41:11 -0300

    Configurar auth module

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { UsersModule } from 'src/users/users.module';
+import { AuthService } from './auth.service';
+import { JWT_SECRET } from './constants';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { LocalStrategy } from './strategies/local.strategy';
+
+@Module({
+  imports: [
+    UsersModule,
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: JWT_SECRET,
+      signOptions: { expiresIn: '3600s' },
+    }),
+  ],
+  providers: [AuthService, JwtStrategy, LocalStrategy],
+})
+export class AuthModule {}

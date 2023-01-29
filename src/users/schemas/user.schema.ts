13a3295f3535228ab9b06a216a8a6d290c788604
Robert0101 <rob.tangled@gmail.com>
2023-01-29 11:51:20 -0300

    add graphql fields + unique index

--- src/users/schemas/user.schema.ts
+++ src/users/schemas/user.schema.ts
@@ -1,23 +1,30 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
+import { Field, ObjectType } from '@nestjs/graphql';
 
 export type UserDocument = User & Document;
 
+@ObjectType()
 @Schema()
 export class User {
-  @Prop()
+  @Prop({ required: true, unique: true, immutable: true, index: true })
+  @Field()
   userId: string;
 
-  @Prop()
+  @Field()
+  @Prop({ required: true, unique: true, immutable: true, index: true })
   email: string;
 
   @Prop()
+  @Field()
   password: string;
 
   @Prop()
+  @Field()
   age: number;
 
   @Prop()
+  @Field({ nullable: true })
   isSubscribed?: boolean;
 }
 
